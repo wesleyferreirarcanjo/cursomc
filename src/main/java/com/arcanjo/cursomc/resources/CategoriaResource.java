8a8384a5b1eb15621764ba93e87c1a82dcf91c37
wesley arcanjo <wesleyferreirarcanjo@gmail.com>
2020-08-19 12:03:47 -0300

    endpoint GET categoria criado

--- src/main/java/com/arcanjo/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/arcanjo/cursomc/resources/CategoriaResource.java
@@ -1,17 +1,30 @@
 package com.arcanjo.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.arcanjo.cursomc.domain.Categoria;
+
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String listar() {
+	public List<Categoria> listar() {
 		
-		return "REST esta funcionando";
+		Categoria categoria = new Categoria(1, "Informatica");
+		Categoria categoria2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(categoria);
+		lista.add(categoria2);
+		
+		return lista;
 		
 	}
 }
