01b8c9f368669c52a6e9fe5e10a2d2167733201c
wesley arcanjo <wesleyferreirarcanjo@gmail.com>
2020-08-19 13:16:01 -0300

    criado repositorio e services para categoria

--- src/main/java/com/arcanjo/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/arcanjo/cursomc/resources/CategoriaResource.java
@@ -3,28 +3,31 @@ package com.arcanjo.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.arcanjo.cursomc.domain.Categoria;
+import com.arcanjo.cursomc.services.CategoriaService;
 
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
-	@GetMapping
-	public List<Categoria> listar() {
+	
+	@Autowired
+	private CategoriaService service;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		Categoria categoria = new Categoria(1, "Informatica");
-		Categoria categoria2 = new Categoria(2, "Escritorio");
+		Categoria obj = service.buscar(id);
 		
-		List<Categoria> lista = new ArrayList<Categoria>();
-		lista.add(categoria);
-		lista.add(categoria2);
-		
-		return lista;
+		return ResponseEntity.ok().body(obj);
 		
 	}
 }
