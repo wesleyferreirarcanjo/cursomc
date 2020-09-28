eaa37ba86a7f375e2a4583f336795610feb80196
wesley arcanjo <wesleyferreirarcanjo@gmail.com>
2020-09-28 11:35:40 -0300

    toString itemPedido e pedido

--- src/main/java/com/arcanjo/cursomc/domain/Pedido.java
+++ src/main/java/com/arcanjo/cursomc/domain/Pedido.java
@@ -1,8 +1,11 @@
 package com.arcanjo.cursomc.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -16,7 +19,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 @Entity
 public class Pedido implements Serializable {
@@ -35,7 +38,7 @@ public class Pedido implements Serializable {
 	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento pagamento;
 	
-	@JsonIgnore
+
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
@@ -105,11 +108,30 @@ public class Pedido implements Serializable {
 		this.itens = itens;
 	}
 
+
 	@Override
 	public String toString() {
-		return "Pedido [id=" + id + ", instante=" + instante + ", pagamento=" + pagamento + ", cliente=" + cliente
-				+ ", enderecoDeEntrega=" + enderecoDeEntrega + "]";
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
+		StringBuilder builder = new StringBuilder();
+		builder.append("Pedido numero: ");
+		builder.append(getId());
+		builder.append(", Instante: ");
+		builder.append(sdf.format(getInstante()));
+		builder.append(", Cliente: ");
+		builder.append(getCliente().getNome());
+		builder.append(", Situacao do pagamento: ");
+		builder.append(getPagamento().getEstadoPagamento().getDescricao());
+		builder.append("\nDetalhes:\n");
+		for(ItemPedido ip : getItens()) {
+			builder.append(ip.toString());
+		}
+		builder.append("Valor total:");
+		builder.append(nf.format(getValorTotal()));
+		
+		return builder.toString();
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
--- src/main/java/com/arcanjo/cursomc/resources/PedidoResource.java
+++ src/main/java/com/arcanjo/cursomc/resources/PedidoResource.java
@@ -32,7 +32,7 @@ public class PedidoResource {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj){
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj) throws IllegalArgumentException, IllegalAccessException{
 		obj = pedidoService.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
