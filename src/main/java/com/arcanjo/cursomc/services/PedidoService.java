165820601e0402c59e3673d5fa23852e4d7efe0e
wesley arcanjo <wesleyferreirarcanjo@gmail.com>
2020-09-28 11:35:40 -0300

    toString itemPedido e pedido

--- src/main/java/com/arcanjo/cursomc/services/PedidoService.java
+++ src/main/java/com/arcanjo/cursomc/services/PedidoService.java
@@ -33,7 +33,11 @@ public class PedidoService {
 	@Autowired
 	private ProdutoService produtoService;
 	
-	@Autowired ItemPedidoRepository itemPedidoRepository;
+	@Autowired 
+	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private ClienteService clienteService;
 	
 	
 	public Pedido find(Integer id)  {
@@ -45,6 +49,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstadoPagamento(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if(obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -56,10 +61,12 @@ public class PedidoService {
 		
 		for(ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
+			ip.setProduto(produtoService.find( ip.getProduto().getId() ));
 			ip.setPreco(produtoService.find( ip.getProduto().getId() ).getPreco() );
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
 	}
 }
