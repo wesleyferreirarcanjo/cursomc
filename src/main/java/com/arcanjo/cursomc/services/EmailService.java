5e92250e923f6d85f87b57594859bf935659d7e5
wesley arcanjo <wesleyferreirarcanjo@gmail.com>
2020-09-28 14:49:16 -0300

    adicionado email html

--- src/main/java/com/arcanjo/cursomc/services/EmailService.java
+++ src/main/java/com/arcanjo/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.arcanjo.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.arcanjo.cursomc.domain.Pedido;
@@ -9,4 +11,8 @@ public interface EmailService {
 	public void sendOrderConfirmationEmail(Pedido obj);
 	
 	public void sendEmail(SimpleMailMessage msg);
+	
+	public void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	public void sendHtmlEmail(MimeMessage msg);
 }
--- src/main/java/com/arcanjo/cursomc/services/MockEmailService.java
+++ src/main/java/com/arcanjo/cursomc/services/MockEmailService.java
@@ -1,5 +1,7 @@
 package com.arcanjo.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
@@ -15,6 +17,13 @@ public class MockEmailService extends AbstractEmailService{
 		LOG.info("Email enviado");
 		
 		
+	}
+	
+	@Override
+	public void sendHtmlEmail (MimeMessage msg) {
+		LOG.info("simulando envio de email html...");
+		LOG.info(msg.toString());
+		LOG.info("Email enviado");
 	}
 
 }
