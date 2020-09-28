3312bf8f9f45216f356f45fd75f59172224d807f
wesley arcanjo <wesleyferreirarcanjo@gmail.com>
2020-09-28 14:49:16 -0300

    adicionado email html

--- src/main/java/com/arcanjo/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/arcanjo/cursomc/services/SmtpEmailService.java
@@ -1,15 +1,21 @@
 package com.arcanjo.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SmtpEmailService extends AbstractEmailService{
 
 	@Autowired
 	private MailSender mailSender;
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
 	
 	private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
 	
@@ -21,5 +27,15 @@ public class SmtpEmailService extends AbstractEmailService{
 		
 		
 	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("simulando envio de email HTML...");
+		javaMailSender.send(msg);
+		LOG.info("Email enviado");
+		
+	}
+	
+	
 
 }
