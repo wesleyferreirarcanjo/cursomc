190b660d05c7a2b05cdace4ca43045e0ea97d626
wesley arcanjo <wesleyferreirarcanjo@gmail.com>
2020-10-07 08:26:10 -0300

    adicionado permissao para clientes so exegarem eles mesmos

--- src/main/java/com/arcanjo/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/arcanjo/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.arcanjo.cursomc.services.exceptions.AuthorizationException;
 import com.arcanjo.cursomc.services.exceptions.DataIntegrityException;
 import com.arcanjo.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -41,5 +42,14 @@ public class ResourceExceptionHandler {
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
+	
+	@ExceptionHandler(AuthorizationException.class)
+	public ResponseEntity<StandardError> dataIntegrity(AuthorizationException e, HttpServletRequest request) {
+
+		StandardError err = new StandardError(HttpStatus.FORBIDDEN.value(), e.getMessage(),
+				System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
+	}
+
 	
 }
