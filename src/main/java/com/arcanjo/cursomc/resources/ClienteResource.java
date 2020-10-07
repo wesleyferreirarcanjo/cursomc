6f4cec53f440474b2e0f2baafd1d0be1f0eb8bbe
wesley arcanjo <wesleyferreirarcanjo@gmail.com>
2020-10-07 19:18:01 -0300

    endpoint encontrar por email criado

--- src/main/java/com/arcanjo/cursomc/resources/ClienteResource.java
+++ src/main/java/com/arcanjo/cursomc/resources/ClienteResource.java
@@ -41,6 +41,15 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(obj);
 
 	}
+	
+	@GetMapping("/email")
+	public ResponseEntity<Cliente> find(@RequestParam(value="value") String email) {
+
+		Cliente obj = clienteService.findByEmail(email);
+		return ResponseEntity.ok().body(obj);
+
+	}
+
 
 	@PostMapping
 	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
--- src/main/java/com/arcanjo/cursomc/services/ClienteService.java
+++ src/main/java/com/arcanjo/cursomc/services/ClienteService.java
@@ -5,7 +5,6 @@ import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -34,43 +33,43 @@ import com.arcanjo.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
-	
+
 	@Autowired
 	private ClienteRepository repo;
-	
+
 	@Autowired
 	private EnderecoRepository enderecoRepo;
-	
+
 	@Autowired
 	private CidadeRepository cidadeRepository;
-	
+
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
-	
+
 	@Autowired
 	private ImageService imageService;
-	
+
 	@Autowired
 	private S3Service s3Service;
-	
+
 	@Value("${img.prefix.client.profile}")
 	private String imgPrefix;
-	
+
 	@Value("${img.profile.size}")
 	private Integer imgSize;
-	
+
 	public Cliente find(Integer id) {
-		
+
 		UserSS user = UserService.authenticated();
-		
-		if(user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
 			throw new AuthorizationException("Acesso negado");
 		}
-		
+
 		Optional<Cliente> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException(String.format("Objeto nao encontrado! Id: %s, Tipo: %s", id, Cliente.class.getName())));
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				String.format("Objeto nao encontrado! Id: %s, Tipo: %s", id, Cliente.class.getName())));
 	}
-	
 
 	@Transactional
 	public Cliente insert(Cliente obj) {
@@ -79,87 +78,100 @@ public class ClienteService {
 		enderecoRepo.saveAll(obj.getEndereco());
 		return obj;
 	}
-	
+
 	public void update(Cliente obj) {
 		Cliente newObj = this.find(obj.getId());
 		this.updataData(newObj, obj);
 		repo.save(newObj);
-		
+
 	}
 
-
-	public void delete (Integer id) {
+	public void delete(Integer id) {
 		this.find(id);
-		
+
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("nao e possivel excluir cliente pois ha pedidos relacionados");
 		}
-			
+
 	}
-	
+
 	public List<Cliente> findAll() {
 		return repo.findAll();
 	}
-	
+
+	public Cliente findByEmail(String email) {
+
+		UserSS user = UserService.authenticated();
+
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !email.equals(user.getUsername())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+
+		Optional<Cliente> obj = repo.findById(user.getId());
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				String.format("Objeto nao encontrado! Id: %s, Tipo: %s", user.getId(), Cliente.class.getName())));
+
+	}
+
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
-		
+
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
-	
+
 	public Cliente fromDto(ClienteDTO obj) {
 		return new Cliente(obj.getId(), obj.getNome(), obj.getEmail(), null, null, null);
 	}
-	
+
 	public Cliente fromDTO(ClienteNewDTO objDto) {
-		
-		Cliente cliente = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipoCliente()), bCryptPasswordEncoder.encode(objDto.getSenha()));
-		
+
+		Cliente cliente = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				TipoCliente.toEnum(objDto.getTipoCliente()), bCryptPasswordEncoder.encode(objDto.getSenha()));
+
 		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
 		cidadeRepository.save(cid);
-		
-		Endereco endereco = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cliente, cid);
-		
+
+		Endereco endereco = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cliente, cid);
+
 		cliente.getEndereco().add(endereco);
 		cliente.getTelefones().add(objDto.getTelefone());
-		
-		if(objDto.getTelefone2() != null) {
-			cliente.getTelefones().add(objDto.getTelefone2());			
+
+		if (objDto.getTelefone2() != null) {
+			cliente.getTelefones().add(objDto.getTelefone2());
 		}
-		
-		if(objDto.getTelefone3() != null) {
-			cliente.getTelefones().add(objDto.getTelefone3());			
-		}	
-		
+
+		if (objDto.getTelefone3() != null) {
+			cliente.getTelefones().add(objDto.getTelefone3());
+		}
+
 		return cliente;
 	}
 
-	
 	private void updataData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
-		
+
 	}
 
-
 	public URI uploadProfilePicture(MultipartFile multipartFile) {
-		
+
 		UserSS user = UserService.authenticated();
-		if(user == null) {
+		if (user == null) {
 			throw new AuthorizationException("Acesso negado");
 		}
-		
+
 		BufferedImage jpgImage = imageService.getJpgImageFromFIle(multipartFile);
 		jpgImage = imageService.cropSquare(jpgImage);
 		jpgImage = imageService.resize(jpgImage, imgSize);
-		
+
 		String filename = imgPrefix + user.getId() + ".jpg";
 		URI uri = s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), filename, "image");
-		
+
 		return uri;
 	}
-
 
 }
