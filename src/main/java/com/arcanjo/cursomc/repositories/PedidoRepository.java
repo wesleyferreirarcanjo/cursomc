61be448ae499ac00fa299885f7265dafb9e54cad
wesley arcanjo <wesleyferreirarcanjo@gmail.com>
2020-10-07 10:05:27 -0300

    cliente somente recupera seus pedidos

--- src/main/java/com/arcanjo/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/arcanjo/cursomc/repositories/PedidoRepository.java
@@ -1,11 +1,20 @@
 package com.arcanjo.cursomc.repositories;
 
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.arcanjo.cursomc.domain.Cliente;
 import com.arcanjo.cursomc.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
+	
+	@Transactional(readOnly = true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 
 }
